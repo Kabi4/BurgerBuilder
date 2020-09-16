d983fcba3347d9c71f838b2e972ca3377db21385
Kushagra Singh <59528754+Kabi4@users.noreply.github.com>
2020-09-16 07:29:51 +0530

    Update Button.js

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import classes from './Button.css';
-
+ 
 const Button = (props) => (<button disabled={props.isdisable} onClick={props.click} className={[classes.Button,classes[props.type]].join(' ')}>{props.children}</button>);
 
 export default Button;
